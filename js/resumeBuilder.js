f4d66b0faa19a17f531c1bf2182c6cebd5b9b4bd
nel gucci <nelgucci28@gmail.com>
2017-08-29 17:04:21 -0400

    revised

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -47,7 +47,7 @@ bio.display = function() {
                 $("#skills").append(formattedSkills);
             }
  	}
-}
+};
 
 bio.display();
 
@@ -158,14 +158,14 @@ work.display = function() {
 // 		logClicks(x,y);
 // });
 
- function inName(name) {
- 	name = name.trim().split(" ");
- 	console.log(name);
- 	name[1]=name[1].toUpperCase();
- 	name[0]=name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
- 	return name[0]+ " "+ name[1];
- }
- $('#main').append(internationalizeButton);
+ // function inName(name) {
+ // 	name = name.trim().split(" ");
+ // 	console.log(name);
+ // 	name[1]=name[1].toUpperCase();
+ // 	name[0]=name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+ // 	return name[0]+ " "+ name[1];
+ // }
+ // $('#main').append(internationalizeButton);
 
 
  projects.display = function() {
